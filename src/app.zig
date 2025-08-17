1b645c402b54f1ecc04bf917c2ea5ff384793094
Geert Fannes <geert.fannes@auro-3d.com>
2025-08-17 14:48:43 +0200

    wip on prot

--- src/app.zig
+++ src/app.zig
@@ -8,7 +8,11 @@ const crypto = @import("crypto.zig");
 pub const Error = error{
     ExpectedIp,
     ExpectedPort,
+    ExpectedHello,
     ExpectedReplicate,
+    ExpectedRun,
+    ExpectedBye,
+    UnknownId,
     NotImplemented,
 };
 
@@ -75,6 +79,14 @@ pub const App = struct {
                 return Error.ExpectedReplicate;
         }
     }
+
+    fn printMessage(self: Self, msg: anytype) !void {
+        if (self.log.level(1)) |w| {
+            try w.print("\nReceived message:\n", .{});
+            var root = rubr.naft.Node.init(w);
+            msg.write(&root);
+        }
+    }
     fn runServer(self: *Self) !void {
         const addr = try self.address();
         if (self.log.level(1)) |w|
@@ -93,31 +105,50 @@ pub const App = struct {
 
             var tr = rubr.comm.TreeReader(std.net.Stream){ .in = connection.stream };
 
-            var replicate: prot.Replicate = undefined;
-            var aa = std.heap.ArenaAllocator.init(self.a);
-            defer aa.deinit();
-            if (!try tr.readComposite(&replicate, 2, aa.allocator()))
-                return Error.ExpectedReplicate;
+            var quit = false;
+            while (!quit) {
+                var aa = std.heap.ArenaAllocator.init(self.a);
+                defer aa.deinit();
 
-            if (self.log.level(1)) |w| {
-                var root = rubr.naft.Node.init(w);
-                replicate.write(&root);
+                const header = try tr.readHeader();
+                switch (header.id) {
+                    prot.Hello.Id => {
+                        var msg: prot.Hello = undefined;
+                        if (!try tr.readComposite(&msg, header.id, {}))
+                            return Error.ExpectedHello;
+                        try self.printMessage(msg);
+                    },
+                    prot.Replicate.Id => {
+                        var msg: prot.Replicate = undefined;
+                        if (!try tr.readComposite(&msg, header.id, aa.allocator()))
+                            return Error.ExpectedReplicate;
+                        try self.printMessage(msg);
+                    },
+                    prot.Run.Id => {
+                        var msg: prot.Run = undefined;
+                        if (!try tr.readComposite(&msg, header.id, aa.allocator()))
+                            return Error.ExpectedRun;
+                        try self.printMessage(msg);
+                    },
+                    prot.Bye.Id => {
+                        var msg: prot.Bye = undefined;
+                        if (!try tr.readComposite(&msg, header.id, {}))
+                            return Error.ExpectedBye;
+                        try self.printMessage(msg);
+
+                        if (self.log.level(1)) |w|
+                            try w.print("Closing connection\n", .{});
+                        quit = true;
+                    },
+                    else => {
+                        try self.log.err("Unknown Id {}\n", .{header.id});
+                        return Error.UnknownId;
+                    },
+                }
             }
         }
     }
     fn runClient(self: *Self) !void {
-        var replicate: prot.Replicate = .{
-            .a = self.a,
-            .base = "tmp",
-            .files = try tree.collectFileStates(std.fs.cwd(), self.a),
-        };
-        defer replicate.deinit();
-
-        if (self.log.level(1)) |w| {
-            var root = rubr.naft.Node.init(w);
-            replicate.write(&root);
-        }
-
         const addr = try self.address();
         if (self.log.level(1)) |w|
             try w.print("Connecting to {}\n", .{addr});
@@ -126,7 +157,35 @@ pub const App = struct {
 
         const tw = rubr.comm.TreeWriter(std.net.Stream){ .out = stream };
 
-        try tw.writeComposite(&replicate, 2);
+        {
+            const T = prot.Hello;
+            try tw.writeComposite(T{ .role = T.Role.Client, .status = T.Status.Pending }, T.Id);
+        }
+
+        {
+            const T = prot.Replicate;
+            var msg = T.init(self.a);
+            defer msg.deinit();
+            msg.base = try msg.a.dupe(u8, "tmp");
+            msg.files = try tree.collectFileStates(std.fs.cwd(), self.a);
+
+            try tw.writeComposite(msg, T.Id);
+        }
+
+        {
+            const T = prot.Run;
+            var msg = T.init(self.a);
+            defer msg.deinit();
+            msg.cmd = try msg.a.dupe(u8, "rake");
+            try msg.args.append(try msg.a.dupe(u8, "ut"));
+
+            try tw.writeComposite(msg, T.Id);
+        }
+
+        {
+            const T = prot.Bye;
+            try tw.writeComposite(T{}, T.Id);
+        }
     }
 
     fn address(self: Self) !std.net.Address {
--- src/prot.zig
+++ src/prot.zig
@@ -5,48 +5,73 @@ const rubr = @import("rubr.zig");
 pub const Error = error{
     ExpectedString,
     ExpectedSize,
+    ExpectedVersion,
+    ExpectedRole,
+    ExpectedStatus,
     ExpectedFileState,
+    ExpectedCmd,
+    ExpectedArg,
     UnexpectedData,
 };
 
 pub const Hello = struct {
     const Self = @This();
+    pub const Id = 2;
+    pub const Role = enum { Client, Server, Broker };
+    pub const Status = enum { Ok, Pending, Fail };
 
-    pub fn init() Self {
-        return Self{};
-    }
-    pub fn deinit(self: *Self) void {
-        _ = self;
+    version: u32 = 0,
+    role: Role,
+    status: Status,
+
+    pub fn write(self: Self, parent: *rubr.naft.Node) void {
+        var node = parent.node("prot.Hello");
+        defer node.deinit();
+        node.attr("version", self.version);
+        node.attr("role", self.role);
+        node.attr("status", self.status);
     }
     pub fn writeComposite(self: Self, tw: anytype) !void {
-        _ = self;
-        _ = tw;
+        try tw.writeLeaf(self.version, 3);
+        try tw.writeLeaf(@intFromEnum(self.role), 3);
+        try tw.writeLeaf(@intFromEnum(self.status), 3);
     }
-    pub fn readComposite(self: *Self, tr: anytype, a: std.mem.Allocator) !void {
-        _ = self;
-        _ = tr;
-        _ = a;
+    pub fn readComposite(self: *Self, tr: anytype, _: void) !void {
+        if (!try tr.readLeaf(&self.version, 3, {}))
+            return Error.ExpectedVersion;
+
+        var u: u32 = undefined;
+
+        if (!try tr.readLeaf(&u, 3, {}))
+            return Error.ExpectedRole;
+        self.role = @enumFromInt(u);
+
+        if (!try tr.readLeaf(&u, 3, {}))
+            return Error.ExpectedStatus;
+        self.status = @enumFromInt(u);
     }
 };
 
 pub const Replicate = struct {
     const Self = @This();
+    pub const Id = 4;
 
     a: std.mem.Allocator,
-    base: []const u8,
+    base: []const u8 = &.{},
     files: tree.FileStates,
 
     pub fn init(a: std.mem.Allocator) Self {
         return Self{ .a = a, .files = tree.FileStates.init(a) };
     }
     pub fn deinit(self: *Self) void {
+        self.a.free(self.base);
         for (self.files.items) |*item|
             item.deinit();
         self.files.deinit();
     }
 
     pub fn write(self: Self, parent: *rubr.naft.Node) void {
-        var node = parent.node("tree.Replicate");
+        var node = parent.node("prot.Replicate");
         defer node.deinit();
         node.attr("base", self.base);
         for (self.files.items) |item|
@@ -79,5 +104,242 @@ pub const Replicate = struct {
 
         if (!try tr.isClose())
             return Error.UnexpectedData;
+    }
+};
+
+pub const Missing = struct {
+    const Self = @This();
+    pub const Id = 6;
+
+    pub fn init() Self {
+        return Self{};
+    }
+    pub fn deinit(self: *Self) void {
+        _ = self;
+    }
+
+    pub fn write(self: Self, parent: *rubr.naft.Node) void {
+        _ = self;
+        var node = parent.node("prot.Missing");
+        defer node.deinit();
+    }
+
+    pub fn writeComposite(self: Self, tw: anytype) !void {
+        _ = self;
+        _ = tw;
+    }
+    pub fn readComposite(self: *Self, tr: anytype, a: std.mem.Allocator) !void {
+        _ = self;
+        _ = tr;
+        _ = a;
+    }
+};
+
+pub const Content = struct {
+    const Self = @This();
+    pub const Id = 8;
+
+    pub fn init() Self {
+        return Self{};
+    }
+    pub fn deinit(self: *Self) void {
+        _ = self;
+    }
+
+    pub fn write(self: Self, parent: *rubr.naft.Node) void {
+        _ = self;
+        var node = parent.node("prot.Content");
+        defer node.deinit();
+    }
+
+    pub fn writeComposite(self: Self, tw: anytype) !void {
+        _ = self;
+        _ = tw;
+    }
+    pub fn readComposite(self: *Self, tr: anytype, a: std.mem.Allocator) !void {
+        _ = self;
+        _ = tr;
+        _ = a;
+    }
+};
+
+pub const Ready = struct {
+    const Self = @This();
+    pub const Id = 10;
+
+    pub fn init() Self {
+        return Self{};
+    }
+    pub fn deinit(self: *Self) void {
+        _ = self;
+    }
+
+    pub fn write(self: Self, parent: *rubr.naft.Node) void {
+        _ = self;
+        var node = parent.node("prot.Ready");
+        defer node.deinit();
+    }
+
+    pub fn writeComposite(self: Self, tw: anytype) !void {
+        _ = self;
+        _ = tw;
+    }
+    pub fn readComposite(self: *Self, tr: anytype, a: std.mem.Allocator) !void {
+        _ = self;
+        _ = tr;
+        _ = a;
+    }
+};
+
+pub const Run = struct {
+    const Self = @This();
+    pub const Id = 12;
+    pub const Args = std.ArrayList([]const u8);
+
+    a: std.mem.Allocator,
+    cmd: []const u8 = &.{},
+    args: Args,
+
+    pub fn init(a: std.mem.Allocator) Self {
+        return Self{ .a = a, .args = Args.init(a) };
+    }
+    pub fn deinit(self: *Self) void {
+        self.a.free(self.cmd);
+        for (self.args.items) |arg|
+            self.a.free(arg);
+        self.args.deinit();
+    }
+
+    pub fn write(self: Self, parent: *rubr.naft.Node) void {
+        var node = parent.node("prot.Run");
+        defer node.deinit();
+        node.attr("cmd", self.cmd);
+        for (self.args.items) |arg|
+            node.attr("arg", arg);
+    }
+
+    pub fn writeComposite(self: Self, tw: anytype) !void {
+        try tw.writeLeaf(self.cmd, 3);
+        try tw.writeLeaf(self.args.items.len, 5);
+        for (self.args.items) |arg|
+            try tw.writeLeaf(arg, 7);
+    }
+    pub fn readComposite(self: *Self, tr: anytype, a: std.mem.Allocator) !void {
+        if (!try tr.readLeaf(&self.cmd, 3, a))
+            return Error.ExpectedCmd;
+
+        var size: usize = undefined;
+        if (!try tr.readLeaf(&size, 5, {}))
+            return Error.ExpectedSize;
+
+        try self.args.resize(size);
+        for (self.args.items) |*arg| {
+            if (!try tr.readLeaf(arg, 7, a))
+                return Error.ExpectedArg;
+        }
+    }
+};
+
+pub const Output = struct {
+    const Self = @This();
+    pub const Id = 14;
+
+    pub fn init() Self {
+        return Self{};
+    }
+    pub fn deinit(self: *Self) void {
+        _ = self;
+    }
+
+    pub fn write(self: Self, parent: *rubr.naft.Node) void {
+        _ = self;
+        var node = parent.node("prot.Output");
+        defer node.deinit();
+    }
+
+    pub fn writeComposite(self: Self, tw: anytype) !void {
+        _ = self;
+        _ = tw;
+    }
+    pub fn readComposite(self: *Self, tr: anytype, a: std.mem.Allocator) !void {
+        _ = self;
+        _ = tr;
+        _ = a;
+    }
+};
+
+pub const Done = struct {
+    const Self = @This();
+    pub const Id = 16;
+
+    pub fn init() Self {
+        return Self{};
+    }
+    pub fn deinit(self: *Self) void {
+        _ = self;
+    }
+
+    pub fn write(self: Self, parent: *rubr.naft.Node) void {
+        _ = self;
+        var node = parent.node("prot.Done");
+        defer node.deinit();
+    }
+
+    pub fn writeComposite(self: Self, tw: anytype) !void {
+        _ = self;
+        _ = tw;
+    }
+    pub fn readComposite(self: *Self, tr: anytype, a: std.mem.Allocator) !void {
+        _ = self;
+        _ = tr;
+        _ = a;
+    }
+};
+
+pub const Bye = struct {
+    const Self = @This();
+    pub const Id = 18;
+
+    pub fn write(self: Self, parent: *rubr.naft.Node) void {
+        _ = self;
+        var node = parent.node("prot.Bye");
+        defer node.deinit();
+    }
+
+    pub fn writeComposite(self: Self, tw: anytype) !void {
+        _ = self;
+        _ = tw;
+    }
+    pub fn readComposite(self: *Self, tr: anytype, _: void) !void {
+        _ = self;
+        _ = tr;
+    }
+};
+
+pub const X = struct {
+    const Self = @This();
+    pub const Id = 20;
+
+    pub fn init() Self {
+        return Self{};
+    }
+    pub fn deinit(self: *Self) void {
+        _ = self;
+    }
+
+    pub fn write(self: Self, parent: *rubr.naft.Node) void {
+        _ = self;
+        var node = parent.node("prot.X");
+        defer node.deinit();
+    }
+
+    pub fn writeComposite(self: Self, tw: anytype) !void {
+        _ = self;
+        _ = tw;
+    }
+    pub fn readComposite(self: *Self, tr: anytype, a: std.mem.Allocator) !void {
+        _ = self;
+        _ = tr;
+        _ = a;
     }
 };
