bf9d5f16385c3d9438aca4e7e6a1c9e3853db081
Geert Fannes <geert.fannes@auro-3d.com>
2025-09-16 20:32:38 +0200

    Switched to Blake3

--- src/crypto.zig
+++ src/crypto.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
 
-// Sha1 checksum 160 bit
-pub const Checksum = [20]u8;
+// Blake3 checksum, we use 128 bit
+pub const Checksum = [16]u8;
 
 pub fn checksum(data: []const u8) Checksum {
-    var sha1 = std.crypto.hash.Sha1.init(.{});
-    sha1.update(data);
-    return sha1.finalResult();
+    var cs: Checksum = undefined;
+    std.crypto.hash.Blake3.hash(data, &cs, .{});
+    return cs;
 }
